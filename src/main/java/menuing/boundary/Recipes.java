d9627774eac1cdaf2ba94e4f005f94e84f37eb65
marc.ribalta <marc.ribalta@eurecat.org>
2018-12-08 19:48:51 +0100

    deleted useless import

--- src/main/java/menuing/boundary/Recipes.java
+++ src/main/java/menuing/boundary/Recipes.java
@@ -7,7 +7,6 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
