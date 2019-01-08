c86c5a60d5a9452efab97643d232032bd6076daa
marc.ribalta <marc.ribalta@eurecat.org>
2019-01-08 19:21:31 +0100

    added background process

--- src/main/java/menuing/boundary/BackgroundJobManager.java
+++ src/main/java/menuing/boundary/BackgroundJobManager.java
@@ -1,13 +1,28 @@
 package menuing.boundary;
 
+import java.io.IOException;
+import java.util.List;
 import javax.ejb.Schedule;
 import javax.ejb.Singleton;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import menuing.entity.User;
 
 @Singleton
 public class BackgroundJobManager {
 
+    @PersistenceContext(name="menuing")
+    EntityManager em;
+    
+    @Inject
+    RecommendedRecipes recommendedRecipes;
+    
     @Schedule(hour="0", minute="0", second="0", persistent=false)
-    public void dailyJob() {
-        // Do your job here which should run every start of day.
+    public void dailyJob() throws IOException {
+        List<User> users = this.em.createNamedQuery(User.FIND_ALL).getResultList();
+        for(User user : users){
+            recommendedRecipes.createRecommendedRecipes(user.getUsername());
+        }
     }
 } 
